5ad37de2e09232446f3e661d0785334132c329c2
Roger Woodroofe <rogyw@yahoo.co.nz>
2015-10-30 18:37:27 +1300

    fix: adjust enemy row alignment and move values into constants

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+var GAME_TILE_WIDTH = 101;
+var GAME_TILE_HEIGHT = 83;
+var GAME_GRID_COLUMNS = 5;
+var GAME_GRID_ROWS = 6;
+var GAME_GRID_ROWS_WATER = 1;
+var GAME_GRID_ROWS_PAVE = 3;
+var GAME_GRID_ROWS_GRASS = 2;
+var ENEMY_SPEED_MAX = 150;
+var ENEMY_SPEED_MIN = 10;
+var ENEMY_Y_OFFSET = 20;
+var ENEMY_WIDTH = GAME_TILE_WIDTH;
+var ENEMY_TOP_MARGIN = 77;
+var ENEMY_HEIGHT = 65;
+var ENEMY_QTY = 6;
+
+
 /**
  * Creates a new Enemy.
  * @constructor
@@ -9,6 +25,9 @@ var Enemy = function() {
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
   this.sprite = 'images/enemy-bug.png';
+
+  this.speedMax = ENEMY_SPEED_MAX;
+  this.speedMin = ENEMY_SPEED_MIN;
 
   // Establish the initial state of the Enemy
   this.reset();
@@ -22,12 +41,16 @@ var Enemy = function() {
  *   all computers.
  */
 Enemy.prototype.update = function(dt) {
+  //The max possible speed of an enemy increases for each row closer to water
+  var rowSpeedMultiplier = (GAME_GRID_ROWS_WATER + GAME_GRID_ROWS_PAVE) - this.row;
 
   //Move the Enemy position
-  this.x = this.x + (this.speed * (4 - this.row) * dt);
-  if (this.x > (101 * 5)) this.reset();
+  this.x = this.x + (this.speed * rowSpeedMultiplier * dt);
 
-  //Check for Collision
+  //Check if Enemy has left the Grid
+  if (this.x > (GAME_TILE_WIDTH * GAME_GRID_COLUMNS)) this.reset();
+
+  //Check for Collision with Player
   this.checkCollision();
 };
 
@@ -38,21 +61,21 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.checkCollision = function() {
   //Calculate player block area
   var pLeft = player.x + player.sideMargin;
-  var pRight = player.x + 101 - player.sideMargin;
+  var pRight = player.x + GAME_TILE_WIDTH - player.sideMargin;
   var pTop = player.y + player.topMargin;
   var pBottom = pTop + player.playerHeight;
 
   //Calculate enemy block area
   var eLeft = this.x;
-  var eRight = this.x + 101;
-  var eTop = this.y + 77;
-  var eBottom = eTop + 65;
+  var eRight = this.x + ENEMY_WIDTH;
+  var eTop = this.y + ENEMY_TOP_MARGIN;
+  var eBottom = eTop + ENEMY_HEIGHT;
 
   //Check for overlap of enemy and player
   if ((eRight > pLeft) && (eLeft < pRight) && (eBottom > pTop) && (eTop < pBottom)) {
-    console.log("**HIT!**");
-    console.log("player = (" + pLeft + "," + pTop + " - " + pRight + "," + pBottom + ")");
-    console.log("enemy = (" + Math.ceil(eLeft) + "," + Math.ceil(eTop) + " - " + Math.ceil(eRight) + "," + Math.ceil(eBottom) + ")");
+    //console.log("**HIT!**");
+    //console.log("player = (" + pLeft + "," + pTop + " - " + pRight + "," + pBottom + ")");
+    //console.log("enemy = (" + Math.ceil(eLeft) + "," + Math.ceil(eTop) + " - " + Math.ceil(eRight) + "," + Math.ceil(eBottom) + ")");
     player.die();
   }
 };
@@ -70,20 +93,20 @@ Enemy.prototype.render = function() {
  * @description:  Establish or reset the initial state of the Enemy
  */
 Enemy.prototype.reset = function() {
-  //The movement speed of the enemy - range 10-150
-  this.speed = Math.floor((Math.random() * 150) + 10);
+  //The movement speed of the enemy
+  this.speed = Math.floor((Math.random() * this.speedMax) + this.speedMin);
 
-  //The row of paving the enemy is allocated to - range 1-3
-  this.row = Math.floor((Math.random() * 3) + 1);
-  this.y = (this.row * 83) - 25;
+  //The row of paving the enemy is allocated to - range 1-GAME_GRID_ROWS_PAVE
+  this.row = Math.floor((Math.random() * GAME_GRID_ROWS_PAVE) + 1);
+  this.y = (this.row * GAME_TILE_HEIGHT) - ENEMY_Y_OFFSET;
 
-  //The column of paving the enemy is allocated to - range 1-5
-  this.column = Math.floor((Math.random() * 5) + 1);
-  this.x = this.column * -101;
+  //The column of paving the enemy is allocated to - range 1-GAME_GRID_COLUMNS
+  this.column = Math.floor((Math.random() * GAME_GRID_COLUMNS) + 1);
+  this.x = this.column * GAME_TILE_WIDTH * -1;
 }
 
 
- /**
+/**
  * Creates a new Player.
  * @constructor
  * @description: represents the player of the game
@@ -150,11 +173,11 @@ Player.prototype.die = function() {
 Player.prototype.reset = function() {
   //The row of grass
   this.row = 5;
-  this.y = this.row * 83;
+  this.y = this.row * GAME_TILE_HEIGHT;
 
   //The column of grass
   this.column = 2;
-  this.x = this.column * 101;
+  this.x = this.column * GAME_TILE_WIDTH;
 
   if (this.score > 20) {
     this.message = "   Well done!"
@@ -194,8 +217,8 @@ Player.prototype.displayScore = function() {
  *   all computers.
  */
 Player.prototype.update = function(dt) {
-  this.x = this.column * 101;
-  this.y = this.row * 83;
+  this.x = this.column * GAME_TILE_WIDTH;
+  this.y = this.row * GAME_TILE_HEIGHT;
   this.displayScore();
 }
 
@@ -249,8 +272,9 @@ Player.prototype.handleInput = function(direction) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
-var enemyCount = 6;
-for (var myEnemies = 0; (myEnemies < enemyCount); myEnemies++) {
+var enemyCount = ENEMY_QTY;
+for (var myEnemies = 0;
+  (myEnemies < enemyCount); myEnemies++) {
   allEnemies.push(new Enemy());
 }
 
